71a4ccc4d7490138a718b252cd75d0575298b9c7
Charly1590 <carlos19.pe@gmail.com>
2020-12-02 11:53:16 -0500

    creacion de metodos dao en el factory y classes que permitan ingresar embresas, categorias, usuarios

--- src/ec/edu/ups/dao/UsuariosDAO.java
+++ src/ec/edu/ups/dao/UsuariosDAO.java
@@ -5,6 +5,5 @@ import java.util.List;
 import ec.edu.ups.entidades.Usuario;
 
 public interface UsuariosDAO extends GenericcDAO<Usuario, Integer>{
-		public abstract Usuario validarU(String correo, String password);
-		public abstract List<Usuario> findU(int id);
+		
 }
--- src/ec/edu/ups/jpa/JPAProductoDAO.java
+++ src/ec/edu/ups/jpa/JPAProductoDAO.java
@@ -1,5 +1,13 @@
 package ec.edu.ups.jpa;
 
-public class JPAProductoDAO {
+import ec.edu.ups.dao.ProductosDao;
+import ec.edu.ups.entidades.Producto;
+import ec.edu.ups.entidades.Usuario;
+
+public class JPAProductoDAO extends JPAGenericDAO<Producto, Integer> implements ProductosDao{
+
+	public JPAProductoDAO() {
+		super(Producto.class);
+	}
 
 }
